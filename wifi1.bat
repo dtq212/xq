8274e130fb73885daf5a9a345988567041951b6a
KNgaiVaCham <dtq21295@gmail.com>
2025-11-14 08:39:41 +0700

    refector

---
+++ wifi1.bat
@@ -0,0 +1,3 @@
+@echo off
+for /f "tokens=*" %%a in ('powershell -Command "Get-NetAdapter | Where-Object {$_.Name -eq 'Wi-Fi'} | Select-Object -ExpandProperty InterfaceIndex"') do set index=%%a
+powershell -Command "Set-NetIPInterface -InterfaceIndex %index% -InterfaceMetric 1"
