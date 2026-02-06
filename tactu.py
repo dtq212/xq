50f82c5c0f8a0e719700a6164a6c963dcca75bdb
KNgaiVaCham <dtq21295@gmail.com>
2026-02-06 11:37:07 +0700

    refector

--- tactu.py
+++ tactu.py
@@ -1422,19 +1422,19 @@ class TacTu:
 
                 if not diachidoituongcanbuffngancham:
                     is_thieuhieuung = not self.moitruong.get_is_cohieuungs((HIEUUNGKYNANG_NGANCHAMDOACH,), macdinh = True, diachicosothongtinnhanvat = diachidoituongdangxemxet, is_hieuungcoloi = 1)
-                    is_quathoigian = time.time() - self._thoidiembuffnganchamgannhat_map.get(idnguoichoidangxemxet, 0) > 60
+                    is_quathoigian = self.moitruong.get_diachicosothongtinnhanvat1() != diachidoituongdangxemxet and self.moitruong.get_is_nguoichoi(diachidoituongdangxemxet) and time.time() - self._thoidiembuffnganchamgannhat_map.get(idnguoichoidangxemxet, 0) > 60
                     if is_thieuhieuung or is_quathoigian:
                         diachidoituongcanbuffngancham = diachidoituongdangxemxet
 
                 if not diachidoituongcanbuffkimcham:
                     is_thieuhieuung = not self.moitruong.get_is_cohieuungs((HIEUUNGKYNANG_KIMCHAMDOACH,), macdinh = True, diachicosothongtinnhanvat = diachidoituongdangxemxet, is_hieuungcoloi = 1)
-                    is_quathoigian = time.time() - self._thoidiembuffkimchamgannhat_map.get(idnguoichoidangxemxet, 0) > 60
+                    is_quathoigian = self.moitruong.get_diachicosothongtinnhanvat1() != diachidoituongdangxemxet and self.moitruong.get_is_nguoichoi(diachidoituongdangxemxet) and time.time() - self._thoidiembuffkimchamgannhat_map.get(idnguoichoidangxemxet, 0) > 60
                     if is_thieuhieuung or is_quathoigian:
                         diachidoituongcanbuffkimcham = diachidoituongdangxemxet
 
                 if not diachidoituongcanbuffcuongthethuat:
                     is_thieuhieuung = not self.moitruong.get_is_cohieuungs((HIEUUNGKYNANG_CUONGTHETHUAT,), macdinh = True, diachicosothongtinnhanvat = diachidoituongdangxemxet, is_hieuungcoloi = 1)
-                    is_quathoigian = time.time() - self._thoidiembuffcuongthethuatgannhat_map.get(idnguoichoidangxemxet, 0) > 60
+                    is_quathoigian = self.moitruong.get_diachicosothongtinnhanvat1() != diachidoituongdangxemxet and self.moitruong.get_is_nguoichoi(diachidoituongdangxemxet) and time.time() - self._thoidiembuffcuongthethuatgannhat_map.get(idnguoichoidangxemxet, 0) > 60
                     if is_thieuhieuung or is_quathoigian:
                         diachidoituongcanbuffcuongthethuat = diachidoituongdangxemxet
 
