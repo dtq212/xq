ed4c246660a1d4725f5ce2d39dadf4b93422e9bc
Thọ Phạm <dtq21295@gmail.com>
2026-08-13 03:23:55 +0700

    abc

--- loop_xq.py
+++ loop_xq.py
@@ -5,7 +5,7 @@ import traceback
 
 import pymem.exception
 
-from hangso_xq import BANDO_CHIENTRUONG, BANDO_CHU, BANDO_TANTHUTHON, TUTHENHANVAT_DUNGIM, BANDOFARMs, BANDOKHONGPKs, HOITHANHPHU, SOLUONGVATPHAMHANHTRANGTOIDA, LAMCAU
+from hangso_xq import BANDO_CHIENTRUONG, BANDO_CHU, BANDO_TANTHUTHON, TUTHENHANVAT_DUNGIM, BANDOFARMs, BANDOKHONGPKs, HOITHANHPHU, SOLUONGVATPHAMHANHTRANGTOIDA, LAMCAU, HACCAU
 from moitruong_xq import MoiTruong
 from tactu_xq import TacTu
 from tienich_xq import phatam
@@ -171,7 +171,7 @@ class LoopChinh:
         #     else:
         #         print("Không tìm thấy dòng Ngoại Công trong mô tả vật phẩm này.")
 
-        # if self.moitruong.get_idnguoichoi() == 59996:
+        # if self.moitruong.get_idnguoichoi() in (59844, 59845):
         #     diachithiensutraodoi = self.moitruong.action_timkiemnhanvat("Thiên Sứ Trao Đổi")
         #     if not diachithiensutraodoi:
         #         return
@@ -180,10 +180,29 @@ class LoopChinh:
         #         return
         #     if self.moitruong.get_khoangcach(diachithiensutraodoi) > 6:
         #         return
-        #     if not self.moitruong.action_timkiemvatphamhanhtrang(LAMCAU):
+        #     if not self.moitruong.action_timkiemvatphamhanhtrang(LAMCAU) or not self.moitruong.action_timkiemvatphamhanhtrang(HACCAU):
         #         return
         #     self.moitruong.action_thucthicaulenh("talk {}# bonus.29".format(hex(idthiensutraodoi).replace("0x", "")), delay = 0)
         #     time.sleep(0.25)
+        #
+        #     iddoituongtuivaibaobo = self.moitruong.action_timkiemvatphamhanhtrang("Bao Vải Ma Bố")
+        #
+        #     if iddoituongtuivaibaobo:
+        #         self.moitruong.action_thucthicaulenh("drop ! {}#1".format(hex(iddoituongtuivaibaobo)).replace("0x", ""), delay = 0)
+        #         time.sleep(0.25)
+        #
+        # if self.moitruong.get_idnguoichoi() in (59844, 59845):
+        #     idtinhnguyendon = self.moitruong.action_timkiemvatphamhanhtrang("Tinh Nguyên Đơn")
+        #     iddoituongbaothudautien = self.moitruong.get_iddoituongbaothudautien()
+        #     if idtinhnguyendon and iddoituongbaothudautien:
+        #         idtinhnguyendon = hex(idtinhnguyendon).replace("0x", "")
+        #         iddoituongbaothudautien = hex(iddoituongbaothudautien).replace("0x", "")
+        #
+        #         caulenh = "pet {}# fuse_yy {}#".format(iddoituongbaothudautien, idtinhnguyendon)
+        #         print()
+        #         self.moitruong.action_thucthicaulenh(caulenh, delay = 0)
+        #         time.sleep(0.25)
+
 
 class LoopPhu:
     def __init__(self, moitruong: MoiTruong, tactu: TacTu, stop: threading.Event):
