15b6b30d55f390382c788da4dc456e03fddd0753
Thọ Phạm <dtq21295@gmail.com>
2026-08-14 10:49:20 +0700

    abc

--- loop_xq.py
+++ loop_xq.py
@@ -5,7 +5,7 @@ import traceback
 
 import pymem.exception
 
-from hangso_xq import BANDO_CHIENTRUONG, BANDO_CHU, BANDO_TANTHUTHON, TUTHENHANVAT_DUNGIM, BANDOFARMs, BANDOKHONGPKs, HOITHANHPHU, SOLUONGVATPHAMHANHTRANGTOIDA, LAMCAU, HACCAU, VATPHAMKHONGBANs, YENTRUONGTHANH2, BANDO_YENTRUONGTHANH3
+from hangso_xq import BANDO_CHIENTRUONG, BANDO_CHU, BANDO_TANTHUTHON, TUTHENHANVAT_DUNGIM, BANDOFARMs, BANDOKHONGPKs, HOITHANHPHU, SOLUONGVATPHAMHANHTRANGTOIDA, LAMCAU, HACCAU, VATPHAMKHONGBANs, YENTRUONGTHANH2, BANDO_YENTRUONGTHANH3, BANDO_YENTRUONGTHANH2, BANDO_YENTRUONGTHANH1
 from moitruong_xq import MoiTruong
 from tactu_xq import TacTu
 from tienich_xq import phatam
@@ -279,7 +279,7 @@ class LoopPhu:
         self.moitruong.action_vohieuhoahookchienquoc2()
         self.tactu.action_muauto()
 
-        if self.moitruong.get_idnguoichoi() in [59503, 59996] and self.moitruong.get_idbandohientai() == BANDO_YENTRUONGTHANH3 and self.tactu._is_tudongvebanrac:
+        if self.moitruong.get_idnguoichoi() in [59503, 59996] and self.moitruong.get_idbandohientai() in [BANDO_YENTRUONGTHANH1, BANDO_YENTRUONGTHANH2, BANDO_YENTRUONGTHANH3] and self.tactu._is_tudongvebanrac:
             for i in range(6, SOLUONGVATPHAMHANHTRANGTOIDA):
                 tenvatphamhanhtrang = self.moitruong.get_tenvatphamhanhtrang(i)
                 if tenvatphamhanhtrang and tenvatphamhanhtrang not in VATPHAMKHONGBANs or tenvatphamhanhtrang in ("Phá Cựu Giáp", ):
